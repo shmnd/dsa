793e4d257b249d1c429b7ef10d973f03a89dd332
Shamnad <112079716+shmnd@users.noreply.github.com>
2026-01-31 19:20:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (65.40%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -1,20 +1,18 @@
-class Solution:
-    def fizzBuzz(self, n: int) -> List[str]:
-        result = []
+class Solution(object):
+    
+    def fizzBuzz(self, n):
+        res = []
         for i in range(1,n+1):
             s = ''
-            
-            if i%3 ==0:
-                s = 'Fizz'
-            if i%5 ==0:
+            if i % 3 == 0:
+                s = "Fizz"
+
+            if i % 5 == 0:
                 s = 'Buzz'
 
-            if i%3==0 and i%5==0:
+            if i % 3 == 0 and i % 5 == 0:
                 s = 'FizzBuzz'
 
-            result.append(s or str(i))
+            res.append(s or str(i))
 
-        return result
-                
-
-        
+        return res
